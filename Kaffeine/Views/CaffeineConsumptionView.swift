ba26f454282a449f2f43714230b6be6df810f76f
GhostyJade <38327334+GhostyJade@users.noreply.github.com>
2021-10-12 21:05:42 +0200

    chore: added gitignore from https://github.com/github/gitignore/blob/master/Swift.gitignore

---
+++ Kaffeine/Views/CaffeineConsumptionView.swift
@@ -0,0 +1,20 @@
+//
+//  CaffeineConsumptionView.swift
+//  Kaffeine
+//
+//  Created by Giulia on 12/10/21.
+//
+
+import SwiftUI
+
+struct CaffeineConsumptionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CaffeineConsumptionView_Previews: PreviewProvider {
+    static var previews: some View {
+        CaffeineConsumptionView()
+    }
+}
